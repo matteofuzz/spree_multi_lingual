187eb106fc3195e6aa74c9f98f69fe74ef54bde4
Matteo Folin <matteo.folin@gmail.com>
2012-06-19 14:17:20 +0200

    patch for create translation of products on fly

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -6,11 +6,12 @@ Spree::Product.class_eval do
   
   private
   
-  def create_default_transalations      
+  def create_default_transalations 
     I18n.available_locales.each do |locale|
       name_locale = self.send("name_#{locale.to_s}")
-      permalink_locale = self.send("permalink_#{locale.to_s}")
-      self.send("permalink_#{locale.to_s}=", locale) if permalink_locale.blank?
-    end  
+      permalink_locale = self.send("permalink_#{locale.to_s}") 
+      self.update_attribute("name_#{locale.to_s}", name) if name_locale.blank?
+      self.update_attribute("permalink_#{locale.to_s}", permalink) if permalink_locale.blank?
+    end 
   end
 end
