a1ebcc1746ec53c24366c3c67a208d2c79259647
Matteo Folin <matteo.folin@gmail.com>
2012-06-19 10:54:52 +0200

    auto compile name and permalink in all locales at creation

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,3 +1,16 @@
 Spree::Product.class_eval do
-  translates :name, :description, :meta_description, :meta_keywords, :permalink
+  translates :name, :description, :meta_description, :meta_keywords, :permalink     
+  
+  before_save :create_default_transalations
+      
+  
+  private
+  
+  def create_default_transalations      
+    I18n.available_locales.each do |locale|
+      name_locale = self.send("name_#{locale.to_s}")
+      permalink_locale = self.send("permalink_#{locale.to_s}")
+      self.send("permalink_#{locale.to_s}=", locale) if permalink_locale.blank?
+    end  
+  end
 end
