ff9560c93abe6a5c3f6df3d457c826d2fff59a05
Matteo Folin <matteo.folin@gmail.com>
2012-06-20 11:34:44 +0200

    revert taxon and taxonomy decorator

--- app/models/taxonomy_decorator.rb
+++ app/models/taxonomy_decorator.rb
@@ -1,8 +1,6 @@
 module Spree
   Taxonomy.class_eval do
-    translates :name   
-    
-    before_save :create_default_transalations     
+    translates :name
 
     private
     def set_name
@@ -11,14 +9,6 @@ module Spree
       else
         self.root = Taxon.create!({ :taxonomy_id => self.id, :name => self.name })
       end
-    end 
-
-    def create_default_transalations 
-      I18n.available_locales.each do |locale|
-        name_locale = self.send("name_#{locale.to_s}")      
-        self.update_attribute("name_#{locale.to_s}", name) if name_locale.blank?      
-      end 
     end
-    
   end
 end
